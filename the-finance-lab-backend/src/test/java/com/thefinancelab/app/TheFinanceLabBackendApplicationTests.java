1ced4ae640000c905b44ec3dd6ad4764e096fc23
Ashray <ashraygupta6@gmail.com>
2025-05-18 01:02:31 +0530

    Test File Pushed

---
+++ the-finance-lab-backend/src/test/java/com/thefinancelab/app/TheFinanceLabBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thefinancelab.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheFinanceLabBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
