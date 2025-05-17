729c4b8512f168a51f4496bc0338f63ecfdd4d8a
Ashray <ashraygupta6@gmail.com>
2025-05-18 01:02:05 +0530

    Initial Commit

---
+++ the-finance-lab-backend/src/main/java/com/thefinancelab/app/TheFinanceLabBackendApplication.java
@@ -0,0 +1,13 @@
+package com.thefinancelab.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheFinanceLabBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheFinanceLabBackendApplication.class, args);
+	}
+
+}
