8b1053e2cf8989a49780ceaba03998a2e2bad210
Ashray <ashraygupta6@gmail.com>
2025-05-18 01:29:40 +0530

    Main Class Updated

--- the-finance-lab-backend/src/main/java/com/thefinancelab/app/TheFinanceLabBackendApplication.java
+++ the-finance-lab-backend/src/main/java/com/thefinancelab/app/TheFinanceLabBackendApplication.java
@@ -2,8 +2,10 @@ package com.thefinancelab.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
 @SpringBootApplication
+@EnableMongoAuditing
 public class TheFinanceLabBackendApplication {
 
 	public static void main(String[] args) {
